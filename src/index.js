8b7fc68cabdde7b120b2b29aad99ada5ad9136dc
Alexandr629-prog <aaskaldovic@gmail.com>
2020-03-22 23:51:48 +0300

    start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+for (let i = 0; i <= bracketsConfig.length - 1;) {
+  let pattern = bracketsConfig[i].join('');
+  if (newstrng.includes(pattern)) {
+  newstring = newstring.replace(pattern, '');
+  i = 0;
+  } else {
+  i = i + 1;
+  }
+  }
+  if (newstring.length == 0) {
+  return true;
+  } else {
+  return false;
+  }
 }
